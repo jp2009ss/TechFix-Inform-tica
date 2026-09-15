76758e1f4c430a9784f02d692dd8c9c1aa9f0edf
Joao Pedro de Souza Santos <1094296@senacminas.edu.br>
2026-09-14 21:05:30 -0300

    Agora Funcionando

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 const btn = document.getElementById('btnCalcular');
 const nomeInput = document.getElementById('nomeCliente');
+const emailInput = document.getElementById('emailCliente');
 const servicoSelect = document.getElementById('servico');
 const resultadoDiv = document.getElementById('resultado');
 const erroP = document.getElementById('erro');
- 
+
 btn.addEventListener('click', () => {
   const nome = nomeInput.value.trim();
+  const email = emailInput.value.trim();
   const opcaoSelecionada = servicoSelect.options[servicoSelect.selectedIndex];
   const preco = opcaoSelecionada.getAttribute('data-preco');
- 
-  if (!nome || !preco) {
+
+  if (!nome || !email || !preco) {
     erroP.style.display = 'block';
     resultadoDiv.textContent = '';
     return;
   }
- 
+
   erroP.style.display = 'none';
- 
+
   const precoFormatado = parseFloat(preco).toLocaleString('pt-BR', {
     style: 'currency',
     currency: 'BRL'
   });
- 
+
   resultadoDiv.innerHTML =
     `Cliente: <strong>${nome}</strong><br>` +
+    `Email: <strong>${email}</strong><br>` +
     `Serviço: <strong>${opcaoSelecionada.value}</strong><br>` +
     `Valor do orçamento: <strong>${precoFormatado}</strong>`;
 });
