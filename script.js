ab5d87134ec80d8129697bd9b54f74f8be18ad34
Joao Pedro de Souza Santos <1094296@senacminas.edu.br>
2026-09-15 21:26:30 -0300

    Comentado e descrição

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ const btn = document.getElementById('btnCalcular');
 const nomeInput = document.getElementById('nomeCliente');
 const emailInput = document.getElementById('emailCliente');
 const servicoSelect = document.getElementById('servico');
+const descricaoInput = document.getElementById('descricao');
 const resultadoDiv = document.getElementById('resultado');
 const erroP = document.getElementById('erro');
 
 btn.addEventListener('click', () => {
   const nome = nomeInput.value.trim();
   const email = emailInput.value.trim();
+  const descricao = descricaoInput.value.trim();
   const opcaoSelecionada = servicoSelect.options[servicoSelect.selectedIndex];
   const preco = opcaoSelecionada.getAttribute('data-preco');
 
-  if (!nome || !email || !preco) {
+  if (!nome || !email || !descricao || !preco) {
     erroP.style.display = 'block';
     resultadoDiv.textContent = '';
     return;
@@ -28,5 +30,6 @@ btn.addEventListener('click', () => {
     `Cliente: <strong>${nome}</strong><br>` +
     `Email: <strong>${email}</strong><br>` +
     `Serviço: <strong>${opcaoSelecionada.value}</strong><br>` +
+    `Descrição: <strong>${descricao}</strong><br>` +
     `Valor do orçamento: <strong>${precoFormatado}</strong>`;
 });
